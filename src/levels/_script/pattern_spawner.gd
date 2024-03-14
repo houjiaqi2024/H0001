f8f2e1ffe75115870098bd1cf8bce83f98dfcbad
houjiaqi <houjiaqi23@proton.me>
2024-03-15 01:03:42 +0800

    update

--- src/levels/_script/pattern_spawner.gd
+++ src/levels/_script/pattern_spawner.gd
@@ -1,19 +1,43 @@
 extends Node2D
 class_name PatternSpawner
 
-const string_to_add = "_extra"
-const ENEMY_INFO := "res://src/utils/enemy_info.json"
+const ENEMY = {
+	SLIME: preload("res://src/enemies/_scenes/baseballguy_mob.tscn"),
+	BAT: preload("res://src/enemies/_scenes/baseballguy_mob.tscn")
+}
+const ENEMY_NAME = {
+	SLIME: "Slime",
+	BAT: "Bat"
+}
 
-# 引用json_utils类
-var json_utils = preload("res://src/utils/_script/json_utils.gd")
-var json_instance = json_utils.new()
+enum {SLIME, BAT}
+enum DIRECTION {PLAYER, MIDDLE, HOVER, UP, RIGHT, DOWN, LEFT}
 
-var enemy_path = json_instance.json_to_dict(ENEMY_INFO)
+var distance = 0.0	# The total distance between point a and point b
+var step = 0.01226 		# The space between each spawned mobType
 
+#Pass array and mob to this function to override base stat
+#Array should be two dimensional with name like ["SPEED", 2000]
+func _set_stats(spawn, newStats):
+	for stat in newStats:
+		spawn.STATS[stat[0]] = stat[1]
 
+#region Random spawning pattern
+func Random(amount, delay): 
+	for x in amount:
+		RandomFullSpawner(randi_range(0, ENEMY.size() -1), [], 1, 0.00)
+		if delay > 0.00:
+			await get_tree().create_timer(delay).timeout
 
-# 创建一个新的字典
-var new_dict = json_instance.add_string_to_dict(enemy_path, string_to_add)
-
-
-
+func RandomFullSpawner(mobType: int, newStats, amount: int, delay: float):
+	for x in range(amount):
+		var spawn = ENEMY[mobType].instantiate()
+		spawn.mobName = ENEMY_NAME[mobType]
+		if newStats.size() != 0:
+			_set_stats(spawn, newStats)
+		%SpawnCircleFollow.progress_ratio = randf()
+		spawn.global_position = %SpawnCircleFollow.global_position
+		add_child(spawn)
+		if delay > 0.00:
+			await get_tree().create_timer(delay).timeout
+#endregion
